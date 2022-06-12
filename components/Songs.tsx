21571a2ecb473c7c4137a0ce227be4b0f764b97a
Atharva J <atharvaj2160@gmail.com>
2022-06-12 19:37:35 +0530

    spacing fix

--- components/Songs.tsx
+++ components/Songs.tsx
@@ -10,7 +10,7 @@ const Songs = () => {
     <div>
       <div className='text-white px-8 grid grid-cols-3'>
         <div className='flex justify-start'>
-          <HashtagIcon className='w-5 h-5 mr-10' />
+          <HashtagIcon className='w-5 h-5 ml-4 mr-14' />
           <p>Title</p>
         </div>
         <div className='flex justify-center'>
